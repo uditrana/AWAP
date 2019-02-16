16115efeda78ae53d510cc567a8b07a784e1b134
eric-clinch <eric.c.clinch@gmail.com>
2019-02-16 14:15:22 -0500

    wrote BFS

--- player/team.py
+++ player/team.py
@@ -5,10 +5,10 @@ Write the __init__ and the step functions. Further explanations
 about these functions are detailed in the wiki.
 
 List your Andrew ID's up here!
-andrewid1
-andrewid2
-andrewid3
-andrewid4
+eclinch
+mpotluri
+uar
+vnbhatia
 """
 from awap2019 import Tile, Direction, State
 
@@ -27,7 +27,31 @@ class Team(object):
         self.team_size = team_size
         self.company_info = company_info
 
-        self.team_name = # Add your team name here!
+        self.team_name = "Thanos Did Nothing Wrong"
+
+    def BFS(self, row, col):
+        target_locations = {c.back_line: c for c in self.unvisited_companies}
+
+        frontier = [(self.board[row][col], row, col, [])]
+        visited = set((row, col))
+        dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        while True:
+            if len(frontier) == 0:
+                return None
+            new_frontier = []
+            for tile, row, col, path in frontier:
+                for drow, dcol in dirs:
+                    new_row = row + drow
+                    new_col = col + dcol
+                    is_valid = (new_row, new_col) not in visited
+                    is_valid &= tile.get_booth() is None
+                    if is_valid:
+                        new_path = path + [(new_row, new_col)]
+                        if (new_row, new_col) in target_locations:
+                            return new_path, target_locations[(new_row, new_col)]
+                        visited.add((new_row, new_col))
+                        new_frontier.append((self.board[new_row][new_col], new_row, new_col, new_path))
+            frontier = new_frontier
 
     def step(self, visible_board, states, score):
         """
@@ -37,4 +61,5 @@ class Team(object):
         are, please look on the wiki.
         """
 
-        pass
+        return [Direction.LEFT, Direction.UP, Direction.RIGHT, Direction.DOWN]
+
